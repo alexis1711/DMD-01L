ff4f1cdf5309d1ed8a22fedfc2a2695c479a6f5b
alexis1711 <alexis101cornejo@gmail.com>
2020-09-09 20:32:33 -0600

    Guia4

---
+++ Guia4/ScriptGuia4.sql
@@ -0,0 +1,19 @@
+create database sv_school_products;
+
+use sv_school_products;
+
+create table products(
+  Categoria nchar(100),
+  Producto nchar(100),
+  Marca nchar(100),
+  Cantidad decimal,
+  Unidad nchar(100),
+  Precio money,
+  Establecimiento nvarchar(4000),
+  Direccion nvarchar (4000),
+  Departamento nchar(100),
+  Municipio nchar(100),
+  [Fecha Sondeo] date
+);
+  
+SELECT*FROM products
